d1cabc4fbae1d6366db7a800d0d8699695269f1e
davidhirtz <david.hirtz@me.com>
2022-02-07 13:51:27 +0100

    RBAC

---
+++ migrations/M220207124544Config.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace davidhirtz\yii2\config\migrations;
+
+use davidhirtz\yii2\skeleton\db\MigrationTrait;
+use Yii;
+use yii\db\Migration;
+
+/**
+ * Class M220207124544Config
+ * @package davidhirtz\yii2\config\migrations
+ * @noinspection PhpUnused
+ */
+class M220207124544Config extends Migration
+{
+    use MigrationTrait;
+
+    /**
+     * @inheritDoc
+     */
+    public function safeUp()
+    {
+        $sourceLanguage = Yii::$app->sourceLanguage;
+
+        $auth = Yii::$app->getAuthManager();
+        $admin = $auth->getRole('admin');
+
+        $configUpdate = $auth->createPermission('configUpdate');
+        $configUpdate->description = Yii::t('config', 'Update website settings', [], $sourceLanguage);
+        $auth->add($configUpdate);
+
+        $auth->addChild($admin, $configUpdate);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function safeDown()
+    {
+        $auth = Yii::$app->getAuthManager();
+        $this->delete($auth->itemTable, ['name' => 'configUpdate']);
+    }
+}
