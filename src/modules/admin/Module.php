50c7fc6d3329de532aac6cf4f115ef813cbf0889
davidhirtz <david.hirtz@me.com>
2024-04-05 21:41:33 +0100

    Version 2.1.7
    Closes #1

--- src/modules/admin/Module.php
+++ src/modules/admin/Module.php
@@ -6,11 +6,12 @@ use davidhirtz\yii2\config\modules\admin\controllers\ConfigController;
 use davidhirtz\yii2\config\modules\admin\models\Config;
 use Yii;
 use yii\helpers\ArrayHelper;
+use davidhirtz\yii2\skeleton\modules\admin\ModuleInterface;
 
 /**
  * @property \davidhirtz\yii2\skeleton\modules\admin\Module $module
  */
-class Module extends \davidhirtz\yii2\skeleton\base\Module
+class Module extends \davidhirtz\yii2\skeleton\base\Module implements ModuleInterface
 {
     /**
      * @var string|null the module display name, defaults to "Settings"
@@ -27,56 +28,40 @@ class Module extends \davidhirtz\yii2\skeleton\base\Module
      */
     public array|string $url = ['/admin/config/update'];
 
-    /**
-     * @var array containing the admin menu items
-     */
-    public array $navbarItems = [];
-
-    /**
-     * @var array containing the panel items
-     */
-    public array $panels = [];
-
-    protected array $defaultControllerMap = [
-        'config' => [
-            'class' => ConfigController::class,
-            'viewPath' => '@config/modules/admin/views/config',
-        ],
-    ];
-
     public function init(): void
     {
         $this->name ??= Yii::t('config', 'Settings');
+        $this->controllerMap = ArrayHelper::merge($this->getCoreControllerMap(), $this->controllerMap);
 
-        if (Yii::$app->has('user')) {
-            if (!$this->navbarItems) {
-                $this->navbarItems = [
-                    'config' => [
-                        'label' => $this->name,
-                        'icon' => 'cogs',
-                        'url' => $this->url,
-                        'active' => ['admin/config/'],
-                        'roles' => [Config::AUTH_CONFIG_UPDATE],
-                        'order' => 100,
-                    ],
-                ];
-            }
-
-            if ($this->panels) {
-                $this->module->panels = array_merge($this->module->panels, $this->panels);
-            } else {
-                $this->module->panels['skeleton'][] = [
-                    'label' => Yii::t('config', 'Settings'),
-                    'url' => ['/admin/config/index'],
-                    'icon' => 'cogs',
-                    'roles' => [Config::AUTH_CONFIG_UPDATE],
-                ];
-            }
-
-            $this->module->navbarItems = array_merge($this->module->navbarItems, $this->navbarItems);
-        }
-
-        $this->module->controllerMap = ArrayHelper::merge(array_merge($this->module->controllerMap, $this->defaultControllerMap), $this->controllerMap);
         parent::init();
+    }
+
+    protected function getCoreControllerMap(): array
+    {
+        return [
+            'config' => [
+                'class' => ConfigController::class,
+                'viewPath' => '@config/modules/admin/views/config',
+            ],
+        ];
+    }
+
+    public function getDashboardPanels(): array
+    {
+        return [];
+    }
+
+    public function getNavBarItems(): array
+    {
+        return [
+            'config' => [
+                'label' => $this->name,
+                'icon' => 'cogs',
+                'url' => $this->url,
+                'active' => ['admin/config/'],
+                'roles' => [Config::AUTH_CONFIG_UPDATE],
+                'order' => 100,
+            ],
+        ];
     }
 }
