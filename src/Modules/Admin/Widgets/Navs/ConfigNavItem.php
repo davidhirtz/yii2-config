4b0ae83250371f5f4cd73084c312cedafb806720
davidhirtz <david.hirtz@me.com>
2026-09-04 09:34:08 +0200

    Menu update

--- src/Modules/Admin/Widgets/Navs/ConfigNavItem.php
+++ src/Modules/Admin/Widgets/Navs/ConfigNavItem.php
@@ -7,13 +7,12 @@ namespace Hirtz\Config\Modules\Admin\Widgets\Navs;
 use Hirtz\Skeleton\I18n\Lang;
 use Hirtz\Config\Modules\Admin\Models\Config;
 use Hirtz\Skeleton\Widgets\Navs\NavItem;
-use Yii;
 
 class ConfigNavItem extends NavItem
 {
     public function __construct(array $config = [])
     {
-        $this->icon ??= 'wrench';
+        //$this->icon ??= 'wrench';
         $this->label ??= Lang::t('config', 'COMMON_SETTINGS');
         $this->order ??= 100;
         $this->roles ??= [Config::AUTH_CONFIG_UPDATE];
